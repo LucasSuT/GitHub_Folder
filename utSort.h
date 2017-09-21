382e05641198614fc70a1c797a4f922bf3a0237f
Lucas <tn00917892@gmail.com>
2017-09-21 18:00:30 +0800

    hw1_new

--- utSort.h
+++ utSort.h
@@ -10,14 +10,14 @@ using namespace std;
 TEST (Sort, sortByIncreasingPerimeter) {
     Rectangle r1(1,1,3,4);
     Circle c1(1,1,3);
-        //印出Rectangle和Circle的數據
+      /*  //印出Rectangle和Circle的數據
       cout<<r1.toString()<<endl;
         cout<<"perimeter="<<r1.perimeter()<<endl;
         cout<<"area="<<r1.area()<<endl;
       cout<<c1.toString()<<endl;
         cout<<"perimeter="<<c1.perimeter()<<endl;
         cout<<"area="<<c1.area()<<endl;
-
+        */
     vertex v1,v2,v3;
     v1.x=0;
     v1.y=0;
@@ -26,11 +26,11 @@ TEST (Sort, sortByIncreasingPerimeter) {
     v3.x=2;
     v3.y=0;
     Triangle t1(v1,v2,v3);
-        //印出Triangle的數據
+    /*    //印出Triangle的數據
       cout<<t1.toString()<<endl;
           cout<<"perimeter="<<t1.perimeter()<<endl;
           cout<<"area="<<t1.area()<<endl;
-      
+    */
     list<Shape *>shapels;
     shapels.push_back (&r1);
     shapels.push_back (&c1);
